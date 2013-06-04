fced7ffc2106fa403f2530acbd612f0d92514b81
Leniy Tsan <m@leniy.org>
2013-06-04 02:11:04 +0000

    init
    git-svn-id: http://plugins.svn.wordpress.org/leniy-tweaks/trunk@722362 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ leniy.php
@@ -0,0 +1,48 @@
+<?php
+/*
+	Plugin Name: Leniy Tweaks
+	Plugin URI: http://blog.leniy.org/leniy-tweaks.html
+	Description: 为中国大陆用户设计的wordpress小工具，适合中文用户习惯
+	Version: 0.1
+	Author: leniy
+	Author URI: http://blog.leniy.org/
+	Text Domain: leniylang
+*/
+
+/*  Copyright 2012 Leniy (m@leniy.org)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as 
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+if ( ! defined( 'LENIY_PLUGIN_DIR' ) ) {
+    define( 'LENIY_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
+}
+
+if ( ! defined( 'LENIY_PLUGIN_URL' ) ) {
+    define( 'LENIY_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
+}
+
+
+//博客统计状态
+//require_once( LENIY_PLUGIN_DIR . 'leniy_blogstat.php' );
+
+//固定WordPress后台管理工具栏
+//require_once( LENIY_PLUGIN_DIR . 'leniy_fixed_adminmenuwrap.php' );
+
+//服务器本地缓存Avatar头像
+//require_once( LENIY_PLUGIN_DIR . 'leniy_local_avatar.php' );
+
+
+
+?>
---
+++ readme.txt
@@ -0,0 +1,39 @@
+=== Leniy Tweaks ===
+Contributors: leniy
+Donate link: http://blog.leniy.org
+Tags: comments,stat,clear,clean,tweaks
+Requires at least: 3.0
+Tested up to: 3.6
+Stable tag: trunk
+License: GPLv3 or later
+License URI: http://www.gnu.org/licenses/gpl.html
+
+为中国大陆用户设计的wordpress小工具，适合中文用户习惯
+
+== Description ==
+
+为中国大陆用户设计的wordpress小工具，适合中文用户习惯
+
+DEMO:[Leniy](http://blog.leniy.org/leniy-tweaks.html "示例页面")
+
+[Donate link](https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=HAENMLDR2UMFJ&lc=US&item_name=Leniy%20Plugins%20Donation&item_number=plugin%2ddonate&currency_code=USD&bn=PP%2dDonationsBF%3abtn_donate_SM%2egif%3aNonHosted)
+
+== Installation ==
+
+1. Copy Leniy-Tweaks.zip to your Wordpress plugins directory, usually `wp-content/plugins/` and unzip the file.  It will create a `wp-content/plugins/Leniy-Tweaks/` directory.
+2. Activate the plugin through the 'Plugins' menu in WordPress.
+
+== Screenshots ==
+
+
+== Changelog ==
+
+[View the change log here.](http://blog.leniy.org/leniy-tweaks.html)
+
+== Frequently Asked Questions ==
+
+[View here](http://blog.leniy.org/leniy-tweaks.html)
+
+== Upgrade Notice ==
+
+[View here](http://blog.leniy.org/leniy-tweaks.html)
