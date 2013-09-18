48760c1ca27384a861f89c02dc9581111ecd4940
Leniy Tsan <m@leniy.org>
2013-09-18 01:41:55 +0000

    update
    git-svn-id: http://plugins.svn.wordpress.org/leniy-tweaks/trunk@774342 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- leniy.php
+++ leniy.php
@@ -3,27 +3,13 @@
 	Plugin Name: Leniy Tweaks
 	Plugin URI: http://blog.leniy.org/leniy-tweaks.html
 	Description: 为中国大陆用户设计的wordpress小工具，适合中文用户习惯
-	Version: 0.3.6
+	Version: 0.4.0
 	Author: leniy
 	Author URI: http://blog.leniy.org/
 	Text Domain: leniylang
 */
 
-/*  Copyright 2012-2013 Leniy (m@leniy.org)
-
-    This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License, version 2, as 
-    published by the Free Software Foundation.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program; if not, write to the Free Software
-    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-*/
+require_once(plugin_dir_path( __FILE__ ).'/inc/leniy_admin_menu.php');
 
 if ( ! defined( 'LENIY_PLUGIN_DIR' ) ) {
     define( 'LENIY_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
